f718a37657ded6c52452e6dcd5ace69235daacbe
Gabriel <gabrielcapraru@gmail.com>
2022-03-17 11:21:58 +0000

    Adding react router and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+//styles
 import './App.css'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
